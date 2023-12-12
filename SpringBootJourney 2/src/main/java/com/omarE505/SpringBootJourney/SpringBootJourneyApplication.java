65b63acf16f4a1ab32b947b32917a8e3360f5abb
Omar Emad <Omareg402@gmail.com>
2023-12-12 14:34:43 +0200

    section 2: spring initializer

---
+++ SpringBootJourney 2/src/main/java/com/omarE505/SpringBootJourney/SpringBootJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.omarE505.SpringBootJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJourneyApplication.class, args);
+	}
+
+}
---
+++ SpringBootJourney 2/src/test/java/com/omarE505/SpringBootJourney/SpringBootJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarE505.SpringBootJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
