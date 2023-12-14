834278dfb69b51e605c29e07db051da04c771a01
Omar Emad <Omareg402@gmail.com>
2023-12-14 13:50:39 +0200

    Section 4 : Creating Spring Boot Project

---
+++ Section 4/ChuckNorisApp/src/main/java/com/omarE505/ChuckNorisApp/ChuckNorisAppApplication.java
@@ -0,0 +1,13 @@
+package com.omarE505.ChuckNorisApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorisAppApplication.class, args);
+	}
+
+}
