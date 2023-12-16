22e37bc1845903ad6f50c42c7cdd9326318f80da
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-12-16 20:59:49 +0200

    Section 6 : Using Properties Source

--- Section 5/DependencyInjection/src/main/java/com/omarE505/DependencyInjection/DependencyInjectionApplication.java
+++ Section 5/DependencyInjection/src/main/java/com/omarE505/DependencyInjection/DependencyInjectionApplication.java
@@ -1,52 +1,57 @@
 package com.omarE505.DependencyInjection;
 
 import com.omarE505.DependencyInjection.controllers.*;
+import com.omarE505.DependencyInjection.datasource.FakeDataSource;
 import com.omarE505.DependencyInjection.services.PrototypeBean;
 import com.omarE505.DependencyInjection.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
 
-		PetController petController = ctx.getBean("petController", PetController.class);
-		System.out.println("--- The Best Pet Is ---");
-		System.out.println(petController.whichPetIsTheBest());
+        PetController petController = ctx.getBean("petController", PetController.class);
+        System.out.println("--- The Best Pet Is ---");
+        System.out.println(petController.whichPetIsTheBest());
 
-		I18NController i18NController = (I18NController) ctx.getBean("i18NController");
-		System.out.println(i18NController.sayHello());
+        I18NController i18NController = (I18NController) ctx.getBean("i18NController");
+        System.out.println(i18NController.sayHello());
 
-		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.println("-------- Primary");
-		System.out.println(myController.sayHello());
+        MyController myController = (MyController) ctx.getBean("myController");
+        System.out.println("-------- Primary");
+        System.out.println(myController.sayHello());
 
-		System.out.println("-------- property");
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
+        System.out.println("-------- property");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        System.out.println(propertyInjectedController.getGreeting());
 
-		System.out.println("-------- Setter");
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
+        System.out.println("-------- Setter");
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+        System.out.println(setterInjectedController.getGreeting());
 
-		System.out.println("-------- Constructor");
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
+        System.out.println("-------- Constructor");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+        System.out.println(constructorInjectedController.getGreeting());
 
-		System.out.println("----------- Bean Scopes -------------");
-		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
-		System.out.println(singletonBean1.getMyScope());
-		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
-		System.out.println(singletonBean2.getMyScope());
+        System.out.println("----------- Bean Scopes -------------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
 
-		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
-		System.out.println(prototypeBean1.getMyScope());
-		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
-		System.out.println(prototypeBean2.getMyScope());
-	}
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getJdbcurl());
+    }
 
 }
--- Section 5/DependencyInjection/src/main/java/com/omarE505/DependencyInjection/config/GreetingServiceConfig.java
+++ Section 5/DependencyInjection/src/main/java/com/omarE505/DependencyInjection/config/GreetingServiceConfig.java
@@ -1,63 +1,75 @@
 package com.omarE505.DependencyInjection.config;
 
+import com.omarE505.DependencyInjection.datasource.FakeDataSource;
 import com.omarE505.DependencyInjection.repositories.EnglishGreetingRepository;
 import com.omarE505.DependencyInjection.repositories.EnglishGreetingRepositoryImpl;
 import com.omarE505.DependencyInjection.services.*;
 import com.springframework.pets.PetService;
 import com.springframework.pets.PetServiceFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 @ImportResource("classpath:omar-config.xml")
 @Configuration
+@PropertySource("classpath:datasource.properties")
 public class GreetingServiceConfig {
 
     @Bean
-    PetServiceFactory petServiceFactory(){
+    FakeDataSource fakeDataSource(@Value("${com.username}") String username, @Value("${com.password}") String password, @Value("${com.jdbcurl}") String jdbcurl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setJdbcurl(jdbcurl);
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        return fakeDataSource;
+    }
+
+    @Bean
+    PetServiceFactory petServiceFactory() {
         return new PetServiceFactory();
     }
 
     @Bean
     @Profile({"dog", "default"})
-    PetService dogPetService(PetServiceFactory petServiceFactory){
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
         return petServiceFactory.getPetService("dog");
     }
 
     @Bean
     @Profile("cat")
-    PetService catPetService(PetServiceFactory petServiceFactory){
+    PetService catPetService(PetServiceFactory petServiceFactory) {
         return petServiceFactory.getPetService("cat");
     }
 
     @Bean
-    EnglishGreetingRepository englishGreetingRepository(){
+    EnglishGreetingRepository englishGreetingRepository() {
         return new EnglishGreetingRepositoryImpl();
     }
 
     @Bean
-    PropertyGreetingsService propertyGreetingsService(){
+    PropertyGreetingsService propertyGreetingsService() {
         return new PropertyGreetingsService();
     }
 
     @Bean
-    SetterGreetingsService setterGreetingsService(){
+    SetterGreetingsService setterGreetingsService() {
         return new SetterGreetingsService();
     }
 
     @Bean
     @Primary
-    PrimaryGreetingsService primaryGreetingsService(){
+    PrimaryGreetingsService primaryGreetingsService() {
         return new PrimaryGreetingsService();
     }
 
     @Bean
     @Profile("EN")
-    I18NEnglishGreetingsService i18nService(EnglishGreetingRepository englishGreetingRepository){
+    I18NEnglishGreetingsService i18nService(EnglishGreetingRepository englishGreetingRepository) {
         return new I18NEnglishGreetingsService(englishGreetingRepository);
     }
 
     @Bean("i18nService")
-    @Profile({"ES","default"})
-    I18NSpanishGreetingsService i18NSpanishGreetingsService(){
+    @Profile({"ES", "default"})
+    I18NSpanishGreetingsService i18NSpanishGreetingsService() {
         return new I18NSpanishGreetingsService();
     }
 
