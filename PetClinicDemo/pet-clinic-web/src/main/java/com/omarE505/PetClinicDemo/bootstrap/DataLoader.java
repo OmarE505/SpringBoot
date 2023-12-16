c84f240add831a09ce4c543b57f1f387442400b3
Omar Emad <Omareg402@gmail.com>
2023-12-16 13:32:08 +0200

    Section 5 : Auto Generate Map IDs

--- PetClinicDemo/pet-clinic-web/src/main/java/com/omarE505/PetClinicDemo/bootstrap/DataLoader.java
+++ PetClinicDemo/pet-clinic-web/src/main/java/com/omarE505/PetClinicDemo/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Omar");
         owner1.setLastName("Emad");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Test");
         owner2.setLastName("test");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners ...!");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Vet");
         vet1.setLastName("1");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Vet");
         vet2.setLastName("2");
 
