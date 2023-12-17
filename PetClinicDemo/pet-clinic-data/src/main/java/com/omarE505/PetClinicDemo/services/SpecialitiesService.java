dfabccf92d7ac36880ccc61cdfdcdc1ed9a95adc
Omar Emad <Omareg402@gmail.com>
2023-12-17 22:23:41 +0200

    Section 7 : create speciality map service

---
+++ PetClinicDemo/pet-clinic-data/src/main/java/com/omarE505/PetClinicDemo/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.omarE505.PetClinicDemo.services;
+
+import com.omarE505.PetClinicDemo.models.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
