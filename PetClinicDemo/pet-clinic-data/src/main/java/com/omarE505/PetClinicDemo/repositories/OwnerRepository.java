9c8abd6ef517dc676c5e6bd6f7c7d5bd07cd3cd9
Omar Emad <Omareg402@gmail.com>
2023-12-18 23:49:19 +0200

    Section 8 : add spring data jpa owner service

--- PetClinicDemo/pet-clinic-data/src/main/java/com/omarE505/PetClinicDemo/repositories/OwnerRepository.java
+++ PetClinicDemo/pet-clinic-data/src/main/java/com/omarE505/PetClinicDemo/repositories/OwnerRepository.java
@@ -3,5 +3,8 @@ package com.omarE505.PetClinicDemo.repositories;
 import com.omarE505.PetClinicDemo.models.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Optional<Owner> findByLastName(String lastName);
 }
