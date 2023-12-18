db544d31b7e40c8c8214007692ffd572f7779e24
Omar Emad <Omareg402@gmail.com>
2023-12-18 23:25:49 +0200

    Section 8 : Convert Owners to JPA entities

--- PetClinicDemo/pet-clinic-data/src/main/java/com/omarE505/PetClinicDemo/models/Person.java
+++ PetClinicDemo/pet-clinic-data/src/main/java/com/omarE505/PetClinicDemo/models/Person.java
@@ -1,7 +1,13 @@
 package com.omarE505.PetClinicDemo.models;
 
-public class Person extends BaseEntity{
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
