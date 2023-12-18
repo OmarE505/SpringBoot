6cb0e0d1a4a3e159579937b10909432faa8b877e
Omar Emad <Omareg402@gmail.com>
2023-12-18 13:08:43 +0200

    Section 8 : Creating Spring Data Repositories

---
+++ Section 7/RecipeProject/src/main/java/com/omarE505/RecipeProject/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.omarE505.RecipeProject.repositories;
+
+import com.omarE505.RecipeProject.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
---
+++ Section 7/RecipeProject/src/main/java/com/omarE505/RecipeProject/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.omarE505.RecipeProject.repositories;
+
+import com.omarE505.RecipeProject.models.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
