469796f6b91c0b37f4712cf2c0c4988d5723541c
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-12-20 11:32:27 +0200

    Section 10 : using mockito mocks

---
+++ Section 10/RecipeProject/src/test/java/com/omarE505/RecipeProject/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.omarE505.RecipeProject.services;
+
+import com.omarE505.RecipeProject.models.Recipe;
+import com.omarE505.RecipeProject.repositories.RecipeRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        Assertions.assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
