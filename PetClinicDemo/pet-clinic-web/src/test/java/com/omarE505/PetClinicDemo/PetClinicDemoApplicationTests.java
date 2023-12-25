1c1299d78694572f42ce264baf31b90e28f95ffd
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-12-25 14:49:34 +0200

    Section 10 : Convert to Junit 5

--- PetClinicDemo/pet-clinic-web/src/test/java/com/omarE505/PetClinicDemo/PetClinicDemoApplicationTests.java
+++ PetClinicDemo/pet-clinic-web/src/test/java/com/omarE505/PetClinicDemo/PetClinicDemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.omarE505.PetClinicDemo;
 
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class PetClinicDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
