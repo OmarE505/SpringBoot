0620461323474c42a6b4bd82efa775816881c02c
Omar Emad <Omareg402@gmail.com>
2023-12-26 20:04:33 +0200

    Section 11 : Create a recipe

--- Section 11/RecipeProject/src/main/java/com/omarE505/RecipeProject/controllers/RecipeController.java
+++ Section 11/RecipeProject/src/main/java/com/omarE505/RecipeProject/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.omarE505.RecipeProject.controllers;
 
+import com.omarE505.RecipeProject.commands.RecipeCommand;
 import com.omarE505.RecipeProject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,19 @@ public class RecipeController {
     public String showById(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
