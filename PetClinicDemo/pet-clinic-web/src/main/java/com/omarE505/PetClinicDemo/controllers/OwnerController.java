00d7d86ba5c80a9af64666ac040973b7ce48e2c2
Omar Emad <Omareg402@gmail.com>
2023-12-27 22:03:25 +0200

    Section 11 : Display Owner Records

--- PetClinicDemo/pet-clinic-web/src/main/java/com/omarE505/PetClinicDemo/controllers/OwnerController.java
+++ PetClinicDemo/pet-clinic-web/src/main/java/com/omarE505/PetClinicDemo/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.omarE505.PetClinicDemo.controllers;
 import com.omarE505.PetClinicDemo.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -26,4 +29,10 @@ public class OwnerController {
         return "notimplemented";
     }
 
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(this.ownerService.findById(ownerId));
+        return mav;
+    }
 }
