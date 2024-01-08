cdc22fb582ed15ed5fc8f0eeed7f712103b144c8
Omar Emad <Omareg402@gmail.com>
2024-01-08 22:56:59 +0200

    Section 23 : going reactive with spring webClient

--- Section 23/spring-rest-client-examples-master/src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
+++ Section 23/spring-rest-client-examples-master/src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
@@ -13,32 +13,38 @@ import org.springframework.web.server.ServerWebExchange;
 @Controller
 public class UserController {
 
-    private final ApiService apiService;
+    private ApiService apiService;
 
     public UserController(ApiService apiService) {
         this.apiService = apiService;
     }
 
     @GetMapping({"", "/", "/index"})
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @PostMapping("/users")
-    public String formPost(Model model, ServerWebExchange serverWebExchange){
+    public String formPost(Model model, ServerWebExchange serverWebExchange) {
 
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+        //MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
 
-        Integer limit = new Integer(map.get("limit").get(0));
+        // Integer limit = new Integer(map.get("limit").get(0));
 
-        log.debug("Received Limit value: " + limit);
+        //log.debug("Received Limit value: " + limit);
         //default if null or zero
-        if(limit == null || limit == 0){
-            log.debug("Setting limit to default of 10");
-            limit = 10;
-        }
+//        if(limit == null || limit == 0){
+//            log.debug("Setting limit to default of 10");
+//            limit = 10;
+//        }
 
-        model.addAttribute("users", apiService.getUsers(limit));
+        // model.addAttribute("users", apiService.getUsers(limit));
+
+        model.addAttribute("users",
+                apiService
+                        .getUsers(serverWebExchange
+                                .getFormData()
+                                .map(data -> new Integer(data.getFirst("limit")))));
 
         return "userlist";
     }
