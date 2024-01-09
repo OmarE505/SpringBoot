97365bc1ce5c4acb257b3b17b5f4c17ecc4e2e56
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2024-01-09 14:17:38 +0200

    Section 24 : Using mapstruct

--- Section 24/spring5-mvc-rest-master/src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ Section 24/spring5-mvc-rest-master/src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -5,7 +5,7 @@ import guru.springfamework.repositories.CategoryRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class Bootstrap implements CommandLineRunner {
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     public Bootstrap(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
