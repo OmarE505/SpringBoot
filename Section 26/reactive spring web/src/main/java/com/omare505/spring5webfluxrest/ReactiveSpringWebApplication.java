935a8ba8d2f212f9201d8ae71ad953ec9f3529e2
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2024-01-10 14:08:15 +0200

    Section 26 : New Spring Boot Project

---
+++ Section 26/reactive spring web/src/main/java/com/omare505/spring5webfluxrest/ReactiveSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.omare505.spring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringWebApplication.class, args);
+    }
+
+}
