9678a82901b381b2f25e0b9a0ab7821280ed0c92
Omar Emad <Omareg402@gmail.com>
2024-01-10 16:30:54 +0200

    Section 26 : implement POST for vendors

--- Section 26/reactive spring web/src/test/java/com/omare505/spring5webfluxrest/controllers/VendorControllerTest.java
+++ Section 26/reactive spring web/src/test/java/com/omare505/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -1,14 +1,18 @@
 package com.omare505.spring5webfluxrest.controllers;
 
+import com.omare505.spring5webfluxrest.domain.Category;
 import com.omare505.spring5webfluxrest.domain.Vendor;
 import com.omare505.spring5webfluxrest.repositories.VendorRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 
 public class VendorControllerTest {
@@ -44,5 +48,20 @@ public class VendorControllerTest {
                 .uri("api/v1/vendors/someid")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void testCreateVendor() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().firstName("Omar").lastName("Emad").build()));
+
+        Mono<Vendor> catToSaveMono = Mono.just(Vendor.builder().firstName("Omar").lastName("Emad2").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(catToSaveMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
