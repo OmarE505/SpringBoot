0d9576ac500f25b88b1d00ddb86eb14186499f1e
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2024-01-13 22:43:11 +0200

    Section 29 : send and receive of jms messages

--- Section 29/sfg-jms-master/src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
+++ Section 29/sfg-jms-master/src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -10,11 +10,11 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig {
 
     public static final String MY_QUEUE = "my-hello-world";
+    public static final String MY_SEND_RCV_QUEUE = "replybacktome";
 
     @Bean
     public MessageConverter messageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
